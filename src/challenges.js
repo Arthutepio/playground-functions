0dec23691390003ba5066fbeba605a82df5ee587
Arthutepio <arthurodriguesr10@gmail.com>
2022-06-27 17:23:42 -0300

    finalizei o desafio 3

--- src/challenges.js
+++ src/challenges.js
@@ -18,10 +18,13 @@ console.log(calcArea(5, 2));
 console.log(calcArea(51, 1));
 
 // Desafio 3
-function splitSentence() {
-  // seu código aqui
+function splitSentence(parametro) {
+  let string = parametro;
+  return string.split(' ');
 }
-
+console.log(splitSentence('go trybe'));
+console.log(splitSentence('vamo que vamo'));
+console.log(splitSentence('foguete'));
 // Desafio 4
 function concatName() {
   // seu código aqui
