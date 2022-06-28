8002c1f9f5cb465ca9f8497ad9f36b4cf8cd8da5
Arthutepio <arthurodriguesr10@gmail.com>
2022-06-27 21:13:01 -0300

    finalizei o desafio 4 e 5

--- src/challenges.js
+++ src/challenges.js
@@ -26,20 +26,36 @@ console.log(splitSentence('go trybe'));
 console.log(splitSentence('vamo que vamo'));
 console.log(splitSentence('foguete'));
 // Desafio 4
-function concatName() {
-  // seu código aqui
-}
 
+function concatName(nome) {
+  let conatenacao = nome[nome.length - 1] + ', ' + nome[0];
+  return conatenacao;
+}
+console.log(concatName(['Lucas', 'Cassiano', 'Ferraz', 'Paolillo']));
+console.log(concatName(['foguete', 'não', 'tem', 'ré']));
+console.log(concatName(['captain', 'my', 'captain']));
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
+function footballPoints(wins, ties) {
+  let vitorias = wins * 3;
+  let empates = ties * 1;
+  let totaoDePontos = vitorias + empates;
+  return totaoDePontos;
 }
-
+console.log(footballPoints(14, 8));
+console.log(footballPoints(1, 2));
+console.log(footballPoints(0, 0));
 // Desafio 6
-function highestCount() {
-  // seu código aqui
+function highestCount(parametro) {
+  let array = parametro;
+  let maiorNum = array[0];
+  
+ 
+  console.log(maiorNum);
 }
 
+console.log(highestCount([9, 1, 2, 3, 9, 5, 7]));
+console.log(highestCount([0, 4, 4, 4, 9, 2, 1]));
+console.log(highestCount([0, 0, 0]));
 // Desafio 7
 function catAndMouse() {
   // seu código aqui
@@ -47,7 +63,6 @@ function catAndMouse() {
 
 // Desafio 8
 function fizzBuzz() {
-  // seu código aqui
 }
 
 // Desafio 9
