9577c267956171381ee0470ddd7f716b4b422c47
Arthutepio <arthurodriguesr10@gmail.com>
2022-06-30 13:42:00 -0300

    finalizei o desafio 13

--- src/challenges2.js
+++ src/challenges2.js
@@ -14,10 +14,26 @@ function triangleCheck(lineA, lineB, lineC) {
 }
 console.log(triangleCheck(10, 14, 8));
 // Desafio 13
-function hydrate() {
-  // seu código aqui
+function hydrate(string) {
+  let regExp = /\d+/g;
+  let stringNumber = string.match(regExp);
+  let array = [];
+  let resultado = 0;
+
+  for (let index in stringNumber) {
+    array.push(parseInt(stringNumber[index]));
+    resultado += array[index];
+  }
+
+  if (resultado > 1) {
+    return resultado + ' copos de água';
+  }else if (resultado === 1){
+    return resultado + " copo de água";
+  }
 }
 
+
+console.log(hydrate('1 cachaça, 5 cervejas e 1 copo de vinho'));
 module.exports = {
   generatePhoneNumber,
   hydrate,
