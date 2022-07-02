c7f40d95cd723490be0f8e44a29e36b60e50aafd
Arthutepio <arthurodriguesr10@gmail.com>
2022-07-02 17:41:03 -0300

    teste no desafio 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,9 +1,32 @@
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
-}
+function generatePhoneNumber2(array) {
+ //verifica se o array tem 11 digitos
+  if (array.length !== 11) {
+    return 'Array com tamanho incorreto.';
+  }
+  //verifica se array tem 3 numeros iguais
+  for (let index of array) {
+    let numerosIguais = 0;
+    for (let index2 of array) {
+      if (index === index2) {
+        numerosIguais += 1;
+      }
+    }
+    
+    //verifica se os números do array estão entre 0 e 9
+    if (index < 0 || index > 9 || numerosIguais > 2) {
+      return 'não é possível gerar um número de telefone com esses valores';
+    }
+  }
+  // traz os numeros do array no formato desejado
+  return '(' + array[0] + '' + array[1] + ')' + ' ' + array[2] + '' + array[3] + '' + array[4] + '' + array[5] + '' + array[6] + '-' + array[7] + '' + array[8] + '' + array[9] + '' + array[10] + '';
 
-// Desafio 12
+}
+generatePhoneNumber2([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]);
+
+
+
+Desafio 12
 function triangleCheck(lineA, lineB, lineC) {
 
   if (lineA < (lineB  + lineC) && lineA > (Math.abs(lineB - lineC))) {
